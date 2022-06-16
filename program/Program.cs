a426edfb2e087016102ff316498c247f48c0c9ab
Pavel <89213158645p@gmail.com>
2022-06-16 17:04:04 +0300

    task 1 completed

--- program/Program.cs
+++ program/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше либо равна 3 символа.
+void methodArray (string [] array)
+{
+    string [] newArray = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        newArray[i] = array[i];
+        Console.Write(newArray[i] + " ");
+    }
+}
+void ShowArray(string [] array)
+{
+    for(int i = 0; i < array.Length; i++)
+    Console.Write(array[i] + ",");
+}
+string[] array = { "qwe", "1313", "kjdgksd", "=)", "fieowp", "Denmark"};
+ShowArray(array);
+Console.WriteLine();
+methodArray(array);
+
